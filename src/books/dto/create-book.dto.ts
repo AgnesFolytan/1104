1e4a14db5f354d5fb50faa9fe3496890eae7c281
AgnesFolytan <folytanagnes@gmail.com>
2024-11-04 12:32:05 +0100

    dto

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,1 +1,27 @@
-export class CreateBookDto {}
+import { IsBoolean, IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreateBookDto {
+    @IsNotEmpty()
+    @IsNumber()
+    id: number
+
+    @IsNotEmpty()
+    @IsString()
+    title: string
+
+    @IsNotEmpty()
+    @IsString()
+    author: string
+
+    @IsNotEmpty()
+    @IsString()
+    isbn: string
+
+    @IsNotEmpty()
+    @IsNumber()
+    publishYear: number
+
+    @IsNotEmpty()
+    @IsBoolean()
+    reserved: boolean
+}
